37cd86d80b1a92af6367fee8afedd3a2d616da9f
dank <dan.kiernan@bjss.com>
2023-01-19 13:05:04 +0000

    added hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -7,8 +9,23 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-   hero: Hero = {
-    id: 1,
-    name: "Windstorm"
+
+  heroes: Hero[] = [];
+
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
   }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes); 
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  constructor(private heroService: HeroService) {}
 }
